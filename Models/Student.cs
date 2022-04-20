86c964f9baff5266429defff19d25d910edfb600
MrMukhtarov <nijattm@gmail.com>
2022-04-21 00:40:25 +0400

    Homework

---
+++ Models/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework21._4._2022.Models
+{
+    class Student
+    {
+        public string FullName { get; set; }
+        public byte Age { get; set; }
+
+        private string _groupNo;
+
+        public string GroupNo
+        {
+            get => _groupNo;
+            set
+            {
+                while (!CheckGroupNo(value))
+                {
+                    Console.WriteLine("Duzgun daxil edin");
+                    value = Console.ReadLine();
+                    break;
+                }
+                _groupNo = value;
+            }
+        }
+        public Student(string groupno, byte age , string fullname)
+        {
+            GroupNo = groupno;
+            Age = age;
+            FullName = fullname;
+        }
+
+        static bool CheckGroupNo(string groupNo)
+        {
+            if (groupNo.Length >= 4)
+            {
+                bool isupper = false;
+                bool isdigit1 = false;
+                bool isdigit2 = false;
+                bool isdigit3 = false;
+
+                foreach (var item in groupNo)
+                {
+                    if (char.IsUpper(groupNo[0]))
+                    {
+                        isupper = true;
+                    }
+                    else if (char.IsDigit(groupNo[1]))
+                    {
+                        isdigit1 = false;
+                    }
+                    else if (char.IsDigit(groupNo[2]))
+                    {
+                        isdigit2 = false;
+                    }
+                    else if (char.IsDigit(groupNo[3]))
+                    {
+                        isdigit3 = false;
+                    }
+                    if (isupper && isdigit1 && isdigit2 && isdigit3)
+                    {
+                        return true;
+                    }
+                }
+                if (isupper || isdigit1 || isdigit2 || isdigit3)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        //static bool CheckFullName(string fullName)
+        //{
+
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Homework21._4._2022.Models;
+using System;
+
+namespace Homework21._4._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("groupno daxil et");
+            string a = Console.ReadLine();
+            Console.WriteLine("Yasinizi daxil edin");
+            string b = Console.ReadLine();
+            byte.TryParse(b, out byte d);
+            Console.WriteLine("Ad Soyadinizi daxil edin");
+            string c = Console.ReadLine();
+            Student student = new Student(a,d,c);
+            
+            
+        }
+    }
+}
